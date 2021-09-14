2427332dbebd5dbcfcde2b21eb2d0ef52d43b564
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-09-14 18:39:20 +0200

    finished gallery clicking on an image

--- app.js
+++ app.js
@@ -13,25 +13,80 @@ function addImagesToGallery(imageObject) {
 
   const galleryCol = document.createElement("div");
   const imgContainer = document.createElement("div");
-  const imageDiv = document.createElement("div");
-  const imageDivNumber = document.createElement("p");
+  const galleryImg = document.createElement("div");
+  const galleryImgNumber = document.createElement("p");
 
   galleryCol.setAttribute("class", "col");
   imgContainer.setAttribute("class", "img-container");
-  imageDiv.setAttribute("class", "gallery-img");
-  imageDivNumber.setAttribute("class", "gallery-img-number");
+  galleryImg.setAttribute("class", "gallery-img");
+  galleryImg.setAttribute("data-number", imageObject.number);
+  galleryImg.addEventListener("click", (e) =>
+    showGalleryImage(e.target.dataset.number)
+  );
+  galleryImgNumber.setAttribute("class", "gallery-img-number");
 
-  imageDiv.style.backgroundImage = `url('${imageObject.thumbnailLink}')`;
-  imageDivNumber.innerText = imageObject.number;
+  galleryImg.style.backgroundImage = `url('${imageObject.thumbnailLink}')`;
+  galleryImgNumber.innerText = imageObject.number;
 
   galleryCol.appendChild(imgContainer);
-  imgContainer.appendChild(imageDiv);
-  imgContainer.appendChild(imageDivNumber);
+  imgContainer.appendChild(galleryImg);
+  imgContainer.appendChild(galleryImgNumber);
 
   galleryRow.appendChild(galleryCol);
 }
 
-const toggleMenu = () => {};
+const toggleMenu = () => {
+  console.log("menu toggled");
+};
+const clickedImageContainer = document.querySelector(
+  "#clicked-image-container"
+);
+
+const overlay = document.querySelector("#overlay");
+
+const togglePageScroll = (neededAction) => {
+  if (neededAction === "show-scroll") {
+    document.body.style.overflow = "visible";
+  } else if (neededAction === "hide-scroll") {
+    document.body.style.overflow = "hidden";
+  }
+};
+
+const toggleOverlay = () => {
+  togglePageScroll();
+
+  const oldState = overlay.style.display;
+  let newState;
+
+  if (oldState === "block") {
+    togglePageScroll("show-scroll");
+    newState = "none";
+  } else {
+    togglePageScroll("hide-scroll");
+    newState = "block";
+  }
+
+  overlay.style.display = newState;
+};
+
+const showGalleryImage = (imageNumber) => {
+  // TODO fetch all images right after opening the page so that they open fast when clicked
+  const { imgLink, imgText } = galleryImages[imageNumber - 1];
+  clickedImageContainer.querySelector("img").setAttribute("src", imgLink);
+  clickedImageContainer.querySelector("p").innerText = imgText;
+  clickedImageContainer
+    .querySelector("img")
+    .addEventListener("click", hideGalleryImage);
+
+  toggleOverlay();
+  clickedImageContainer.style.display = "flex";
+};
+
+const hideGalleryImage = () => {
+  clickedImageContainer.style.display = "none";
+  toggleOverlay();
+};
+
 document.querySelector("button").addEventListener("click", toggleMenu);
 
 galleryImages.forEach((image, index) => {
--- gallery-images.js
+++ gallery-images.js
@@ -1,36 +1,38 @@
 export const galleryImages = [
   {
     thumbnailLink: "./assets/images/gallery/thumbnails/1.JPG",
-    mainImageLink: "./assets/images/gallery/gallery/1.JPG",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+    imgLink: "./assets/images/gallery/1.JPG",
+    imgText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
   },
   {
     thumbnailLink: "./assets/images/gallery/thumbnails/2.JPG",
-
-    mainImageLink: "./assets/images/gallery/2.JPG",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+    imgLink: "./assets/images/gallery/2.JPG",
+    imgText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
   },
   {
     thumbnailLink: "./assets/images/gallery/thumbnails/3.JPG",
-
-    mainImageLink: "./assets/images/gallery/3.JPG",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+    imgLink: "./assets/images/gallery/3.JPG",
+    imgText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
   },
   {
     thumbnailLink: "./assets/images/gallery/thumbnails/4.JPG",
-
-    mainImageLink: "./assets/images/gallery/4.JPG",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+    imgLink: "./assets/images/gallery/4.JPG",
+    imgText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
   },
   {
     thumbnailLink: "./assets/images/gallery/thumbnails/5.JPG",
-
-    mainImageLink: "./assets/images/gallery/5.JPG",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+    imgLink: "./assets/images/gallery/5.JPG",
+    imgText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
   },
   {
     thumbnailLink: "./assets/images/gallery/thumbnails/6.JPG",
-    mainImageLink: "./assets/images/gallery/6.JPG",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
+    imgLink: "./assets/images/gallery/6.JPG",
+    imgText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting",
   },
 ];
