6c10d8aef85de20f0f515a8c374c37c1b6518779
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-10-23 22:06:15 +0200

    added the new launcher icons

--- launcher-icons.js
+++ launcher-icons.js
@@ -1,26 +1,26 @@
 const launcherImages = [
   {
-    imgLink: "./assets/images/1.png",
+    imgLink: "./assets/logos/superapp.svg",
     hrefValue: "./assets/images/1.png",
   },
   {
-    imgLink: "./assets/images/2.png",
+    imgLink: "./assets/logos/club.svg",
     hrefValue: "./assets/images/2.png",
   },
   {
-    imgLink: "./assets/images/3.png",
+    imgLink: "./assets/logos/digital.svg",
     hrefValue: "./assets/images/3.png",
   },
   {
-    imgLink: "./assets/images/4.png",
+    imgLink: "./assets/logos/express.svg",
     hrefValue: "./assets/images/4.png",
   },
   {
-    imgLink: "./assets/images/5.png",
+    imgLink: "./assets/logos/systems.svg",
     hrefValue: "./assets/images/5.png",
   },
   {
-    imgLink: "./assets/images/6.png",
+    imgLink: "./assets/logos/mafia.svg",
     hrefValue: "./assets/images/6.png",
   },
 ];
