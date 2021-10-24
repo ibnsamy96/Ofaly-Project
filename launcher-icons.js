dd20f87afd7201a820ad4f1305e377f216d48a83
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-10-24 06:22:06 +0200

    enhanced css and changes naming

--- launcher-icons.js
+++ launcher-icons.js
@@ -1,26 +1,26 @@
 const launcherIcons = [
   {
-    imgLink: "./assets/logos/superapp.svg",
+    iconLink: "./assets/logos/superapp.svg",
     hrefValue: "https://ofaly.com/superapp",
   },
   {
-    imgLink: "./assets/logos/club.svg",
+    iconLink: "./assets/logos/club.svg",
     hrefValue: "https://ofaly.com/club",
   },
   {
-    imgLink: "./assets/logos/digital.svg",
+    iconLink: "./assets/logos/digital.svg",
     hrefValue: "https://ofaly.com/digital",
   },
   {
-    imgLink: "./assets/logos/express.svg",
+    iconLink: "./assets/logos/express.svg",
     hrefValue: "https://ofaly.com/express",
   },
   {
-    imgLink: "./assets/logos/systems.svg",
+    iconLink: "./assets/logos/systems.svg",
     hrefValue: "https://ofaly.com/systems",
   },
   {
-    imgLink: "./assets/logos/mafia.svg",
+    iconLink: "./assets/logos/mafia.svg",
     hrefValue: "https://ofaly.com/mafia",
   },
 ];
