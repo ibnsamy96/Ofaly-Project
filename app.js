cc8cad049676fd5661a3cda6daea66152b463631
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-10-24 06:23:15 +0200

    removed unused html and updated the js file for the basic functionalities

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // console.log = () => {}; // to hide all logs
 
-// Holds the document width => helpful to be checked to maintain responsiveness
-let lastWidth;
+// ------------------- General Purpose Functions ------------------- //
 
 // returns the current document width
 function getDocumentWidth() {
@@ -12,107 +11,139 @@ function getDocumentWidth() {
   );
 }
 
+// ------------------------ Updating Clock ------------------------ //
+
+const clockSpan = document.querySelector("#clock");
+
+// Checks if the hours/minutes number is one digit number
+const isItOneDigitNumber = (number) => String(number).split("").length === 1;
+
+// gets the new time and renders the minutes and hours
+const updateTime = () => {
+  const time = new Date();
+  const hours = isItOneDigitNumber(time.getHours())
+    ? `0${time.getHours()}`
+    : time.getHours();
+  const minutes = isItOneDigitNumber(time.getMinutes())
+    ? `0${time.getMinutes()}`
+    : time.getMinutes();
+
+  clockSpan.innerText = `${hours}:${minutes}`;
+};
+
+// update time right after the document is loaded and every second
+updateTime();
+setInterval(updateTime, 1000);
+
+// ------------------------ Adding Icons ------------------------ //
+
+// Holds the document width => helpful to be checked to maintain responsiveness
+let lastWidth;
+
+// /*
+//  returns element height based on its width and its specified aspect-ratio
+//  => helpful to replace aspect-ratio css property as it doesn't work on Safari
+// */
+// function getElementHeightByCalculatingAspectRatio({ element, mainRatio }) {
+//   lastWidth = getDocumentWidth();
+
+//   const elementWidth = parseFloat(
+//     window.getComputedStyle(element).getPropertyValue("width")
+//   );
+//   const elementHeight = elementWidth * (1 / mainRatio);
+
+//   console.log(elementWidth);
+//   console.log(elementHeight);
+
+//   return elementHeight;
+// }
+
+let lastClickedIcon;
+// const iconsContainersObjects = [];
 /*
- returns element height based on its width and its specified aspect-ratio
- => helpful to replace aspect-ratio css property as it doesn't work on Safari
-*/
-function getElementHeightByCalculatingAspectRatio({ element, mainRatio }) {
-  lastWidth = getDocumentWidth();
-
-  const elementWidth = parseFloat(
-    window.getComputedStyle(element).getPropertyValue("width")
-  );
-  const elementHeight = elementWidth * (1 / mainRatio);
-
-  console.log(elementWidth);
-  console.log(elementHeight);
-
-  return elementHeight;
-}
-
-let lastClickedImage;
-const imgsContainersObjects = [];
-/*
-takes an imageObject:{ index , hrefValue , imgLink } as an argument, creates element for the image
+takes an iconObject:{ index , hrefValue , iconLink } as an argument, creates element for the icon
 and and adds it to the gallery section in the page
 */
-function addImagesToLauncher(imageObject) {
-  const launcherRow = document.querySelector("#launcher .container .row");
+function addIconsToLauncher(iconObject) {
+  const iconsSection = document.querySelector(
+    "#launcher-inner-container > section"
+  );
 
-  const launcherCol = document.createElement("div");
-  const imgContainer = document.createElement("a");
-  const launcherImg = document.createElement("div");
+  const iconContainerAnchor = document.createElement("a");
+  const iconImg = document.createElement("img");
 
-  launcherCol.setAttribute("class", "col");
-  imgContainer.setAttribute("class", "img-container");
-  imgContainer.setAttribute("href", imageObject.hrefValue);
-  imgContainer.setAttribute("data-index", imageObject.index);
+  iconContainerAnchor.setAttribute("href", iconObject.hrefValue);
 
-  launcherImg.setAttribute("class", "launcher-img");
-  launcherImg.style.backgroundImage = `url('${imageObject.imgLink}')`;
-  launcherImg.addEventListener("click", (e) => {
+  const iconID = iconObject.hrefValue.split("/").pop() + "-logo";
+  iconContainerAnchor.setAttribute("id", iconID);
+
+  iconImg.setAttribute("src", iconObject.iconLink);
+
+  iconContainerAnchor.addEventListener("click", (e) => {
     e.preventDefault();
     try {
-      lastClickedImage.style.borderColor = "transparent";
+      lastClickedIcon.classList.remove("selected");
     } catch (error) {}
-    lastClickedImage = e.target;
-    lastClickedImage.style.borderColor = "black";
+
+    lastClickedIcon =
+      e.target.tagName === "A" ? e.target : e.target.parentElement;
+    lastClickedIcon.classList.add("selected");
+
     setTimeout(() => {
-      window.location.href = imageObject.hrefValue;
+      // TODO uncomment the next line
+      // window.location.href = iconObject.hrefValue;
     }, 200);
   });
 
-  launcherCol.appendChild(imgContainer);
-  imgContainer.appendChild(launcherImg);
+  iconContainerAnchor.appendChild(iconImg);
+  iconsSection.appendChild(iconContainerAnchor);
 
-  launcherRow.appendChild(launcherCol);
-
-  // specifying launcher images aspect-ratio
-  if (!CSS.supports("aspect-ratio: 1")) {
-    console.log("no aspect-ratio support");
-    const elementObject = {
-      element: imgContainer,
-      mainRatio: 1,
-    };
-    imgContainer.style.height = `${getElementHeightByCalculatingAspectRatio(
-      elementObject
-    )}px`;
-    imgsContainersObjects.push(elementObject);
-  }
+  // // specifying launcher icons aspect-ratio
+  // if (!CSS.supports("aspect-ratio: 1")) {
+  //   console.log("no aspect-ratio support");
+  //   const elementObject = {
+  //     element: iconContainerAnchor,
+  //     mainRatio: 1,
+  //   };
+  //   iconContainerAnchor.style.height = `${getElementHeightByCalculatingAspectRatio(
+  //     elementObject
+  //   )}px`;
+  // iconsContainersObjects.push(elementObject);
+  // }
 }
 
-// adds all images from 'launcherImages' to the launcher
-launcherImages.forEach((image, index) => {
+// adds all icons from 'launcherIcons' to the launcher
+launcherIcons.forEach((icon, index) => {
   console.log(index);
-  const imageObject = { index, ...image };
-  addImagesToLauncher(imageObject);
+  const iconObject = { index, ...icon };
+  addIconsToLauncher(iconObject);
 });
 
-// if images isn't 3,6,9,12... then add 'margin:auto' to the last row images
-if (launcherImages.length % 3 !== 0) {
-  console.log(launcherImages);
-  const imagesTempVar = [
-    ...document.querySelectorAll("#launcher .row .col:nth-of-type(3n-2)"),
-  ];
+// // if icons isn't 3,6,9,12... then add 'margin:auto' to the last row icons
+// if (launcherIcons.length % 3 !== 0) {
+//   console.log(launcherIcons);
+//   const iconsTempVar = [
+//     ...document.querySelectorAll("#launcher .row .col:nth-of-type(3n-2)"),
+//   ];
 
-  const lastImageContainer =
-    imagesTempVar[imagesTempVar.length - 1].querySelector(".img-container");
+//   const lastIconContainer =
+//     iconsTempVar[iconsTempVar.length - 1].querySelector(".icon-container");
 
-  console.log(lastImageContainer);
-  lastImageContainer.classList.add("m-auto");
-}
+//   console.log(lastIconContainer);
+//   lastIconContainer.classList.add("m-auto");
+// }
 
-// if no aspect-ratio support, then add event listener to window to update images height using getElementHeightByCalculatingAspectRatio()
-if (!CSS.supports("aspect-ratio: 1")) {
-  window.addEventListener("resize", () => {
-    if (lastWidth !== getDocumentWidth()) {
-      imgsContainersObjects.forEach((elementObject) => {
-        elementObject[
-          "element"
-        ].style.height = `${getElementHeightByCalculatingAspectRatio(
-          elementObject
-        )}px`;
-      });
-    }
-  });
-}
+// // if no aspect-ratio support, then add event listener to window to update icons height using getElementHeightByCalculatingAspectRatio()
+// if (!CSS.supports("aspect-ratio: 1")) {
+//   window.addEventListener("resize", () => {
+//     if (lastWidth !== getDocumentWidth()) {
+//       iconsContainersObjects.forEach((elementObject) => {
+//         elementObject[
+//           "element"
+//         ].style.height = `${getElementHeightByCalculatingAspectRatio(
+//           elementObject
+//         )}px`;
+//       });
+//     }
+//   });
+// }
