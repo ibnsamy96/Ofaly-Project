e8ae1a12a7491c36567da832eb19d2983b3bc0f2
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-10-26 03:38:07 +0200

    fixed clicking issue

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ function addIconsToLauncher(iconObject) {
     lastClickedIcon.classList.add("selected");
 
     setTimeout(() => {
-      // window.location.href = iconObject.hrefValue;
+      window.location.href = iconObject.hrefValue;
     }, 200);
   });
 
