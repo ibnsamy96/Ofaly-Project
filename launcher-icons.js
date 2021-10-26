2ba2f2ce4cfa4afa28b2d0eacbb1218cbd6706cc
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-10-26 03:31:26 +0200

    fixed css issues with last item:after

--- launcher-icons.js
+++ launcher-icons.js
@@ -23,6 +23,20 @@ const launcherIcons = [
     iconLink: "./assets/logos/mafia.svg",
     hrefValue: "https://ofaly.com/mafia",
   },
+  ,
+  {
+    iconLink: "./assets/logos/mafia.svg",
+    hrefValue: "https://ofaly.com/mafia",
+  },
+  ,
+  {
+    iconLink: "./assets/logos/mafia.svg",
+    hrefValue: "https://ofaly.com/mafia",
+  },
+  {
+    iconLink: "./assets/logos/mafia.svg",
+    hrefValue: "https://ofaly.com/mafia",
+  },
   {
     iconLink: "./assets/logos/mafia.svg",
     hrefValue: "https://ofaly.com/mafia",
